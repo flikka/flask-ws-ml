ca21740999390ba839974318db83cecef0e61971
Kristian Flikka <kristian.flikka@gmail.com>
2017-08-18 13:20:36 +0200

    Added port as env variable, removed web config

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 from flask_restful import Resource, Api
 
+port = os.environ['FLASK_PORT']
 app = Flask(__name__)
 api = Api(app)
 
@@ -11,4 +13,4 @@ class HelloWorld(Resource):
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    app.run(port=80)
+    app.run(port=port)
